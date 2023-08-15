812979be50174793c44d0f800f81f61d4a887d7e
CaioCDJ <caiodjesus1@hotmail.com>
2023-08-14 23:08:45 -0300

    plugins update

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,29 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "adelarsq/neofsharp.vim",
+    event = { "BufEnter *.fsx", "BufEnter *.fs" }
+  },
+  {
+    "jlcrochet/vim-razor",
+    event = { "BufEnter *.razor", "BufEnter *.cshtml" }
+  },
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup {
+        groups = { -- table: default groups
+          'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+          'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+          'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+          'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+        },
+        extra_groups = {},   -- table: additional groups that should be cleared
+        exclude_groups = {}, -- table: groups you don't want to clear
+      }
+    end,
+    cmd = "TransparentEnable",
+    event = "VeryLazy"
+  }
 }
